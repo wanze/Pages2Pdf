d2c39d1c35a9814388ad4dcc2dc01e5b2318ffbd
dsdsdsdsdsds <dsdsdsdsdsds@users.noreply.github.com>
2018-03-18 12:32:00 +0100

    Add option for odd/even headers and footers

---
+++ default_templates/_footer-even.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Pages2Pdf default even footer markup
+ * Styles defined in styles.css file
+ *
+ * You can use {PAGENO} for the current page number and {nbpg} for the total of pages
+ *
+ */
+?>
+
+<!-- output -->
+<hr>
+<div class="footer">Pages2Pdf Default Even Footer / Created on <?= date('Y-m-d') ?> / Page {PAGENO} of {nbpg}</div>
---
+++ default_templates/_header-even.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Pages2Pdf default even header markup
+ * Styles defined in styles.css file
+ *
+ * You can use {PAGENO} for the current page number and {nbpg} for the total of pages
+ *
+ */
+?>
+
+<div class="head">Default Even Header</div>
+Created with Processwire and Pages2Pdf
+<hr>
